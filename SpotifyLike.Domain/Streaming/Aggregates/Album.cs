9f67dfddd5670c7a0764e95a20eeca2d7888ec4a
Thiago Barbosa <thiagfb@hotmail.com>
2023-12-09 11:38:56 -0300

    Criação do projeto streaming e dos objetos musica

---
+++ SpotifyLike.Domain/Streaming/Aggregates/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyLike.Domain.Streaming.Aggregates
+{
+    public class Album
+    {
+        public Guid Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public List<Musica> Musica { get; set; } = new List<Musica>();
+
+        public void AdicionarMusica(Musica musica) => this.Musica.Add(musica);
+
+        public void AdicionarMusica(List<Musica> musica) => this.Musica.AddRange(musica);
+    }
+}
---
+++ SpotifyLike.Domain/Streaming/Aggregates/Musica.cs
@@ -0,0 +1,22 @@
+﻿using SpotifyLike.Domain.Streaming.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyLike.Domain.Streaming.Aggregates
+{
+    public class Musica
+    {
+        public Guid Id { get; set; }
+
+        public String Nome { get; set; }
+
+        /// <summary>
+        /// A música tem duração de minutos ou segundos? 1 minuto é igual a 60 segundos.
+        /// A classe Duracao então irá fazer a conversão de minutos para segundos.
+        /// </summary>
+        public Duracao Duracao { get; set; }
+    }
+}
